af9c0ed2b07cdba7140e0e619cba88c6a3e7b7c9
Dayeon Kim <hamoey@naver.com>
2024-09-12 15:04:09 +0900

    feat: 카카오 애드핏 추가

--- frontend/src/views/NoticeMain.vue
+++ frontend/src/views/NoticeMain.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="container">
     <Header />
+
+    <!-- 카카오 애드센스 광고 영역 -->
+    <div class="ads-container">
+      <ins class="kakao_ad_area" style="display:none;"
+           data-ad-unit="DAN-vGxhtmQFpbiRdNfb"
+           data-ad-width="728"
+           data-ad-height="90"></ins>
+    </div>
+
     <main class="main-content">
       <NoticeList />
     </main>
+
+    <!-- 카카오 애드센스 광고 영역 -->
+    <div class="ads-container-footer">
+      <ins class="kakao_ad_area" style="display:none;"
+           data-ad-unit="DAN-cSgq62orTBdhY5z6"
+           data-ad-width="320"
+           data-ad-height="50"></ins>
+    </div>
+
     <Footer />
   </div>
 </template>
@@ -31,9 +49,19 @@ export default {
 }
 
 .main-content {
-  padding-top: 100px;
+  padding-top: 20px;
   padding-bottom: 50px;
   max-width: 1080px;
   margin: 0 auto;
+}
+
+.ads-container {
+  margin-top: 90px;
+  text-align: center;
+}
+
+.ads-container-footer {
+  margin-bottom: 10px;
+  text-align: center;
 }
 </style>
